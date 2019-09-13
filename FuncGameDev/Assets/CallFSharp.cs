5fc922ec1874f803307d7bc405e4bba9a1d4e4f8
August Dailey <daileyab@rose-hulman.edu>
2019-09-13 01:50:51 -0400

    Initializing FSharp dll + references for unity

---
+++ FuncGameDev/Assets/CallFSharp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using FSharpUnityTutorial;
+
+public class CallFSharp : MonoBehaviour
+{
+    void Start()
+    {
+        FSharpMath math = new FSharpMath();
+        Debug.Log("C# " + math.Add(20, 22).ToString());
+    }
+}
---
+++ FuncGameDev/Assets/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CSharpMath
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
