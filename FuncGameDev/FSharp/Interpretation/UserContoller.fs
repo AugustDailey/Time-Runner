6f78add81a4c018c4b4487e449c4fdc7faea87db
August B Dailey <daileyab@rose-hulman.edu>
2019-10-01 20:44:13 -0400

    First stab at usercontroller. Works without calling functionality

---
+++ FuncGameDev/FSharp/Interpretation/UserContoller.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharp.Interpretation
+
+open UnityEngine
+
+type UserControllerScript() =
+    inherit MonoBehaviour()
+    member this.Update() =
+        if Input.GetKeyDown(KeyCode.RightArrow)
+            then Debug.Log("Player moved right")
+            elif Input.GetKeyDown(KeyCode.LeftArrow)
+                then Debug.Log("Player moved left")
+                elif Input.GetKeyDown(KeyCode.UpArrow)
+                    then Debug.Log("Player moved up")
+                    elif Input.GetKeyDown(KeyCode.DownArrow)
+                        then Debug.Log("Player moved down")
+
