b49c963cdc95e89d67d680c2c6473a483ed51752
August B Dailey <daileyab@rose-hulman.edu>
2019-10-01 20:49:25 -0400

    No more else if

--- FuncGameDev/FSharp/Interpretation/UserContoller.fs
+++ FuncGameDev/FSharp/Interpretation/UserContoller.fs
@@ -6,11 +6,11 @@ type UserControllerScript() =
     inherit MonoBehaviour()
     member this.Update() =
         if Input.GetKeyDown(KeyCode.RightArrow)
-            then Debug.Log("Player moved right")
-            elif Input.GetKeyDown(KeyCode.LeftArrow)
-                then Debug.Log("Player moved left")
-                elif Input.GetKeyDown(KeyCode.UpArrow)
-                    then Debug.Log("Player moved up")
-                    elif Input.GetKeyDown(KeyCode.DownArrow)
-                        then Debug.Log("Player moved down")
+            then Debug.Log("Player moved right. Call move right function here.")
+        if Input.GetKeyDown(KeyCode.LeftArrow)
+            then Debug.Log("Player moved left. Call move left function here.")
+        if Input.GetKeyDown(KeyCode.UpArrow)
+            then Debug.Log("Player moved up. Call move up function here.")
+        if Input.GetKeyDown(KeyCode.DownArrow)
+            then Debug.Log("Player moved down. Call move down function here.")
 
