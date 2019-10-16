0fd928b2379b61ae5e260ceef3433d6afca7fced
August B Dailey <daileyab@rose-hulman.edu>
2019-10-16 17:15:19 -0400

    Integrate layers, player controls specifically

--- FuncGameDev/FSharp/Command/CommandList.fs
+++ FuncGameDev/FSharp/Command/CommandList.fs
@@ -1,6 +1,6 @@
 module Commands
 
-let mutable commands = [1; 2; 3]
+let mutable commands : (int -> obj) list = []
 
 let addCommand x = 
     commands <- x::commands
@@ -8,7 +8,3 @@ let addCommand x =
 let executeAllCommands commands = 
     // Looping through list to execute commands and ignoring subsequent output list
     List.map (fun x -> printfn "Executing element %d" x) commands |> ignore
-
-addCommand 4
-addCommand 5
-executeAllCommands commands;;
