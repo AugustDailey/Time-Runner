b764bfdb27821fa1ff64169a430118a1414ec5fc
Matthew Lyons <lyonsmj@rose-hulman.edu>
2019-10-16 20:11:48 -0400

    Add GameState composite data structure
    The GameState structure contains every other data structure necessary to create the game. At any given moment, a GameState will define all information necessary to reconstruct the game from scratch, though typically it will be used to update the entities in Unity.

---
+++ FuncGameDev/FSharp/Data/GameState.fs
@@ -0,0 +1,12 @@
+﻿module GameState
+
+type T = {
+    players: PlayerData.T list;
+    enemies: EnemyData.T list;
+    weapons: WeaponData.T list;
+    items: ItemData.T list;
+    projectiles: ProjectileData.T list;
+    level: LevelData.T;
+    gamedata: GameData.T;
+    id: int // the next created entity will have this id
+}
