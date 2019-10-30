f13847459854b83e043a11ff5aa42e60b26d051f
August Dailey <37079788+AugustDailey@users.noreply.github.com>
2019-10-29 20:29:38 -0500

    Update Player position in Unity (#13)
    * Added PlayerUpdateScript and StateManager
    
    * Modified the update position function to update based on interpretation fetch
    
    * Pass in Id to move a specific player

---
+++ FuncGameDev/FSharp/Unity/PlayerUpdater.fs
@@ -0,0 +1,19 @@
+﻿namespace FSharp.Unity
+
+open UnityEngine
+
+type PlayerUpdateScript() =
+    inherit MonoBehaviour()
+
+    [<SerializeField>]
+    let mutable gO = null
+
+    member this.Start() =
+        gO = (GameObject) null
+
+    member this.Update() =
+        this.UpdatePosition()
+
+    member this.UpdatePosition() =
+        gO.transform.position <- StateManager.GetPosition 0
+
