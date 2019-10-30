73678b38d3a6e3dba99e58fea3086734a7085e13
August B Dailey <daileyab@rose-hulman.edu>
2019-10-29 21:23:44 -0400

    Remove mutables and type

--- FuncGameDev/FSharp/Services/InputConfigurationService.fs
+++ FuncGameDev/FSharp/Services/InputConfigurationService.fs
@@ -3,33 +3,31 @@
 open System.IO
 open System
 
-type T() =
+let mutable PlayersToControls : Collections.Map<int, ControlModel.T> = Map.ofList[]
 
-    let mutable PlayersToControls : Collections.Map<int, ControlModel.T> = Map.ofList[]
+let AddControlsToMap (stream : StreamReader) (player : string) =
+    let playerNumber = player |> int
+    let parsedUp = stream.ReadLine()
+    let parsedDown = stream.ReadLine()
+    let parsedLeft = stream.ReadLine()
+    let parsedRight = stream.ReadLine()
+    PlayersToControls.Add(playerNumber, {right = parsedRight; left = parsedLeft; up = parsedUp; down = parsedDown})
+    ()
 
-    member x.AddControlsToMap (stream : StreamReader) (player : string) =
-        let playerNumber = player |> int
-        let parsedUp = stream.ReadLine()
-        let parsedDown = stream.ReadLine()
-        let parsedLeft = stream.ReadLine()
-        let parsedRight = stream.ReadLine()
-        PlayersToControls.Add(playerNumber, {right = parsedRight; left = parsedLeft; up = parsedUp; down = parsedDown})
-        ()
+let Read =
 
-    member x.Read =
+    // Read in a file
+    use stream = new StreamReader @"C:\Game_Controls.txt"
 
-        // Read in a file
-        use stream = new StreamReader @"C:\Game_Controls.txt"
-
-        // Continue reading while valid lines.
-        let mutable valid = true
-        while (valid) do
-            let line = stream.ReadLine()
-            if (line = null) then
-                valid <- false
-            else
-                if (line |> Seq.forall Char.IsDigit)
-                    then x.AddControlsToMap stream line
+    // Continue reading while valid lines.
+    let valid = true
+    while (valid) do
+        let line = stream.ReadLine()
+        if (line = null) then
+            ()
+        else
+            if (line |> Seq.forall Char.IsDigit)
+                then AddControlsToMap stream line
                 
 
     
