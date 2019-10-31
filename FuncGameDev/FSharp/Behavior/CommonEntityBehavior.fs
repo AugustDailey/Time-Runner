04cdcae8bfcac1f650e5121579b5af0ca82bb56b
EKim55 <37079871+EKim55@users.noreply.github.com>
2019-10-30 22:25:31 -0400

    Add movement functionality and edit command list and interpreter to fit the infrastructure

--- FuncGameDev/FSharp/Behavior/CommonEntityBehavior.fs
+++ FuncGameDev/FSharp/Behavior/CommonEntityBehavior.fs
@@ -1,4 +1,7 @@
 ﻿module CommonEntityBehavior
+
+open System
+
 //Examples of common entities that would use these functions:
 //players, enemies, items, projectiles
 //Anything with a position uses these
@@ -7,26 +10,51 @@
 //Move entity in direction indicated by degrees
 //eid: int = id of entity
 //degrees: float = angle to move, 0 is north, moves clockwise
+//delta: float = the amount of time passed
 //gs: GameState = the current gamestate
 //returns a function that takes a gamestate and returns a gamestate
-let move eid degrees (gs : obj) =
-    gs
+let move eid (degrees:float) (delta:float) (gs : GameState.T) =
+    let entity = gs.entities.TryFind(eid)
+    let radians = degrees * Math.PI / 180.0
+    match entity with
+    | None -> gs
+    | _ -> 
+        let position = entity.Value.position
+        let distance = (float) entity.Value.speed * delta;
+        let newPosition = ((fst position + distance * (Math.Sin radians)), (snd position + distance * (Math.Cos radians)))
+        let newEntity = { entity.Value with position = newPosition }
+        let newMap = gs.entities.Add(eid, newEntity)
+        { gs with entities = newMap }
 
 //Move entity a set amount in each direction
 //eid: int = id of entity (1-4)
 //xy: float*float = amount to move in x and y directions
 //gs: GameState = the current gamestate
 //returns a function that takes a gamestate and returns a gamestate
-let moveBy eid xy gs =
-    gs
+let moveBy eid xy (gs: GameState.T) =
+    let entity = gs.entities.TryFind(eid)
+    match entity with
+    | None -> gs
+    | _ -> 
+        let position = entity.Value.position
+        let newPosition = ((fst position + fst xy), (snd position + snd xy))
+        let newEntity = { entity.Value with position = newPosition }
+        let newMap = gs.entities.Add(eid, newEntity)
+        { gs with entities = newMap }
     
 //Move entity to an absolute position
 //eid: int = id of entity
 //xy: float*float = position to move to
 //gs: GameState = the current gamestate
 //returns a function that takes a gamestate and returns a gamestate
-let moveTo eid xy gs = 
-    gs
+let moveTo eid xy (gs: GameState.T) = 
+    let entity = gs.entities.TryFind(eid)
+    match entity with
+    | None -> gs
+    | _ -> 
+        let newEntity = { entity.Value with position = xy }
+        let newMap = gs.entities.Add(eid, newEntity)
+        { gs with entities = newMap }
 
 //Handle collision
 //eid1: int = id of first colliding entity
--- FuncGameDev/FSharp/Interpretation/Interpreter.fs
+++ FuncGameDev/FSharp/Interpretation/Interpreter.fs
@@ -1,6 +1,6 @@
 ﻿module Interpreter
 
-let MovePlayerRight id = CommonEntityBehavior.move id 0.0 |> Commands.addCommand
-let MovePlayerLeft id = CommonEntityBehavior.move id 180.0 |> Commands.addCommand
-let MovePlayerUp id = CommonEntityBehavior.move id 90.0 |> Commands.addCommand
-let MovePlayerDown id = CommonEntityBehavior.move id 270.0 |> Commands.addCommand
+let MovePlayerRight id = CommonEntityBehavior.move id (1.0/60.0) 0.0 |> Commands.addCommand
+let MovePlayerLeft id = CommonEntityBehavior.move id (1.0/60.0) 180.0 |> Commands.addCommand
+let MovePlayerUp id = CommonEntityBehavior.move id (1.0/60.0) 90.0 |> Commands.addCommand
+let MovePlayerDown id = CommonEntityBehavior.move id (1.0/60.0) 270.0 |> Commands.addCommand
