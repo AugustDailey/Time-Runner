81c1a66178887bc1f665bb71523479f5e7fc0572
EKim55 <37079871+EKim55@users.noreply.github.com>
2019-12-10 10:01:46 -0500

    Add enemy weapon behavior

--- FuncGameDev/FSharp/Behavior/EnemyBehavior.fs
+++ FuncGameDev/FSharp/Behavior/EnemyBehavior.fs
@@ -4,5 +4,17 @@
 //eid: int = id of enemy
 //gs: GameState = the current gamestate
 //returns a function that takes a gamestate and returns a gamestate
-let useWeapon eid gs =
+let useWeapon eid (gs: GameState.T) =
+    let entityOption = gs.entities.TryFind(eid)
+    match entityOption with
+    | _ ->
+        let entity = entityOption.Value.entity
+        match entity with
+        | EntityType.Weapon weapon -> 
+            let weaponType = weapon.weaponType
+            match weaponType with
+            | WeaponData.Category.Melee -> printfn "Player Using Melee"
+            | WeaponData.Category.Ranged -> printfn "Player Using Ranged"
+            | WeaponData.Category.Roll -> printfn "Player Using Roll"
+            | WeaponData.Category.Active -> printfn "Player Using Active"
     gs
