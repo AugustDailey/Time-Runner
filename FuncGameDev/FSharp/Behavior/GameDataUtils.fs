bc59be7334badaa0e609ad889de29da94034a4a8
EKim55 <37079871+EKim55@users.noreply.github.com>
2019-12-10 19:52:44 -0500

    Add functions for increasing and decreasing timer

---
+++ FuncGameDev/FSharp/Behavior/GameDataUtils.fs
@@ -0,0 +1,20 @@
+﻿module GameDataUtils
+
+// Increases time and returns a new GameState with updated time
+let increaseTime deltaTime (gs: GameState.T) =
+    let gameData = gs.gamedata
+    let newTime = gameData.time + deltaTime
+    let newGameData = { gameData with time = newTime }
+    let newGS = { gs with gamedata = newGameData}
+    newGS
+
+// Decreases time and returns a new GameState with updated time
+let decreaseTime deltaTime (gs: GameState.T) =
+    let gameData = gs.gamedata
+    let newTime = gameData.time - deltaTime
+    match newTime with
+    | newTime when newTime < 0.0 -> newTime = 0.0
+
+    let newGameData = { gameData with time = newTime }
+    let newGS = { gs with gamedata = newGameData}
+    newGS
