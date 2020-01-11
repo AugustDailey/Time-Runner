0bc3e078071ad1ad207ed99517a721dbc007f3ca
August B Dailey <daileyab@rose-hulman.edu>
2020-01-11 16:32:02 -0500

    Add PlayerCollision Script and Updated prefabs with collider components

---
+++ FuncGameDev/FSharp/Unity/Collision/PlayerCollision.fs
@@ -0,0 +1,34 @@
+﻿namespace FSharp.Unity.Collision
+
+open UnityEngine
+
+type PlayerCollision()=
+    inherit MonoBehaviour()
+
+    member this.Start() = 
+        Debug.Log(this.GetComponent<Rigidbody2D>())
+
+    member this.OnTriggerEnter2D (col : Collision2D) =
+        
+        if (col.gameObject.name = "Player(Clone)") then
+            // Player Collision
+            Debug.Log("Player collided with another Player")
+            ()
+        
+        if (col.gameObject.name = "Enemy(Clone)") then
+            // Enemy Collision
+            Debug.Log("Player collided with Enemy")
+            ()
+
+        if (col.gameObject.name = "Item(Clone)") then
+            // Item Collision
+            Debug.Log("Player collided with Item")
+            ()
+
+        if (col.gameObject.name = "Weapon(Clone)") then
+            // Weapon Collision
+            Debug.Log("Player collided with Weapon")
+            ()
+         
+        
+        
