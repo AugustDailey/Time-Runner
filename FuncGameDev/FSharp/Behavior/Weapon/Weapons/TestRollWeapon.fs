601d36872a779dd04b6a1a7f48037721e05d78d2
EKim55 <37079871+EKim55@users.noreply.github.com>
2020-01-14 22:01:33 -0500

    Add Test Melee Weapon and Roll

---
+++ FuncGameDev/FSharp/Behavior/Weapon/Weapons/TestRollWeapon.fs
@@ -0,0 +1,13 @@
+﻿module TestRollWeapon
+
+// The Test Roll Weapon is a roll move
+// It moves the user 5 units in the direction they are facing
+
+let attack (weaponData: WeaponData.T) (commonEntityData: CommonEntityData.T) xy degrees (gs: GameState.T) = 
+    let newEntity = { commonEntityData with position = (fst commonEntityData.position + 5.0 * cos degrees, snd commonEntityData.position + 5.0 * sin degrees) }
+    let newGS = { gs with entities = gs.entities.Add(commonEntityData.id, commonEntityData)}
+    newGS;
+
+let behavior = {
+    WeaponBehaviorType.attack = attack;
+}
