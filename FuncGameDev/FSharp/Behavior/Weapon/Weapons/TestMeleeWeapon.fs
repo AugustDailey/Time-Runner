5f062a1fe40794874f92bd3289e16213fabe75ea
EKim55 <37079871+EKim55@users.noreply.github.com>
2020-01-14 22:01:33 -0500

    Add Test Melee Weapon and Roll

---
+++ FuncGameDev/FSharp/Behavior/Weapon/Weapons/TestMeleeWeapon.fs
@@ -0,0 +1,12 @@
+﻿module TestMeleeWeapon
+
+// The Test Melee Weapon is a weapon
+// It performs a basic slashing attack
+
+let attack (weaponData: WeaponData.T) (commonEntityData: CommonEntityData.T) xy degrees (gs: GameState.T) = 
+    let newGS = TestProjectile.spawn weaponData xy 0 degrees gs;
+    newGS;
+
+let behavior = {
+    WeaponBehaviorType.attack = attack;
+}
