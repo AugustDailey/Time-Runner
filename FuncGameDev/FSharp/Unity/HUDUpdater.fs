42d2418b664a7619181b378b00c8c7f773d1c501
trdavello <localmgr@davelltr-5510.rose-hulman.edu>
2020-01-16 02:56:55 -0500

    Add HUD to display time

---
+++ FuncGameDev/FSharp/Unity/HUDUpdater.fs
@@ -0,0 +1,15 @@
+﻿namespace FSharp.Unity
+
+open UnityEngine
+
+
+// This script must be placed on a gameobject called HUD with a TextMesh
+type HUDUpdater() = 
+    inherit MonoBehaviour()
+    
+    member this.Update() = 
+        let camera = GameObject.Find("Main Camera")
+        let txt = this.GetComponent<TextMesh>()
+        txt.text <- "Time: 100" // grab time from where ever it is
+        this.transform.position <- new Vector3(0.0f, 4.5f, 5.0f) + camera.transform.position
+        ()
