9c59093f6aef9ba4427e8a5d95d2459179a6dcdb
trdavello <localmgr@davelltr-5510.rose-hulman.edu>
2020-01-18 00:43:25 -0500

    Add everything to HUD scene because Gameplay one is randomly missing all objects

--- FuncGameDev/FSharp/Unity/HUDUpdater.fs
+++ FuncGameDev/FSharp/Unity/HUDUpdater.fs
@@ -10,12 +10,8 @@ type HUDUpdater() =
     member this.Update() = 
         let camera = GameObject.Find("Main Camera")
         let txt = this.GetComponent<TextMesh>()
-<<<<<<< HEAD
         let time : int = int GameState.instance.gamedata.time
         let time_string : string = string time
         txt.text <- "Time: " + time_string
-=======
-        txt.text <- "Time: 100" // grab time from where ever it is
->>>>>>> 472bb3b2ebd6328ec85f97455568f82fb7ed3536
         this.transform.position <- new Vector3(0.0f, 4.5f, 5.0f) + camera.transform.position
         ()
