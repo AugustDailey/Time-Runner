ef40d5751badfe3d47aaec305878049a9440c69e
Matthew Lyons <lyonsmj@rose-hulman.edu>
2020-01-20 13:34:48 -0500

    Add player->projectile collision

--- FuncGameDev/FSharp/Data/ProjectileData.fs
+++ FuncGameDev/FSharp/Data/ProjectileData.fs
@@ -4,4 +4,6 @@ type T = { damage: int;
            effects: EffectData.T list;
            lifespan: float;
            degrees: float; 
-           behaviorID: int; }
+           behaviorID: int;
+           health: int; // number of things it can hit before it dies
+           team: int } // 0 is friendly, 1 is enemy, 2+ can damage anyone
