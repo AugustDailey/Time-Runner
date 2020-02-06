8da69d1657d6a1010e45a4ad6875f1feb84f3552
August B Dailey <daileyab@rose-hulman.edu>
2020-02-06 17:32:57 -0500

    Call CameraInterpreter Update instead

--- FuncGameDev/FSharp/Unity/Update.fs
+++ FuncGameDev/FSharp/Unity/Update.fs
@@ -15,7 +15,7 @@ type Updater() =
         ()
 
     member this.Update() =
-        CameraUpdater.update
+        CameraInterpreter.updateCamera
         Time.deltaTime |> float |> GameDataUtils.decreaseTime |> Commands.addCommand
         GameState.instance.entities |> Map.iter UserController.tryQueryInput
         GameState.instance.entities |> Map.iter EnemyAIScript.callEnemyAI
