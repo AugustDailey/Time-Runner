9d73c9904994b3e5bb42ad46c10e0cbec77477ce
August B Dailey <daileyab@rose-hulman.edu>
2020-02-05 20:09:02 -0500

    Ignore Z axis and leverage camera behavior

--- FuncGameDev/FSharp/Data/CameraData.fs
+++ FuncGameDev/FSharp/Data/CameraData.fs
@@ -1,7 +1,7 @@
 ﻿module CameraData
 
-type T = { position: float * float * float;
-           rotation: float * float * float;
-           scale: float * float * float;
+type T = { position: float * float;
+           rotation: float * float;
+           scale: float * float;
            width: float;
            height: float }
--- FuncGameDev/FSharp/Unity/Updater/CameraUpdater.fs
+++ FuncGameDev/FSharp/Unity/Updater/CameraUpdater.fs
@@ -4,32 +4,20 @@ open UnityEngine
 
 let update =
     // update camera
-    let camera = UnityEngine.Camera.main
+    let camera = Camera.main
     let transform = camera.transform
 
     let cPosX = transform.position.x |> float
     let cPosY = transform.position.y |> float
-    let cPosZ = transform.position.z |> float
+    let pos = (cPosX, cPosY)
 
     let cRotX = transform.rotation.x |> float
     let cRotY = transform.rotation.y |> float
-    let cRotZ = transform.rotation.z |> float
+    let rot = (cRotX, cRotY)
 
     let cScaleX = transform.localScale.x |> float
     let cScaleY = transform.localScale.y |> float
-    let cScaleZ = transform.localScale.z |> float
+    let scale = (cScaleX, cScaleY)
 
-    let cd : CameraData.T = { 
-        position = (cPosX, cPosY, cPosZ);
-        rotation = (cRotX, cRotY, cRotZ);
-        scale = (cScaleX, cScaleY, cScaleZ);
-        width = 20.0;
-        height = 10.0 }
-    let gd : GameData.T = {
-        time = GameState.instance.gamedata.time;
-        floor = GameState.instance.gamedata.floor;
-        camera = cd }
-
-    let gs = GameState.instance
-    GameState.instance <- { gs with gamedata = gd}
+    CameraBehavior.updateCamera pos, rot, scale, 20.0, 10.0
     ()
