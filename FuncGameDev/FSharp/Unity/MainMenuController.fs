a55ee858b00bb32429dcf541a30ffb00f0d3abcd
August B Dailey <daileyab@rose-hulman.edu>
2020-02-09 14:14:04 -0500

    Add main menu and hookup to gameplay scene

---
+++ FuncGameDev/FSharp/Unity/MainMenuController.fs
@@ -0,0 +1,14 @@
+﻿namespace FSharp.Unity
+
+open UnityEngine
+open UnityEngine.SceneManagement
+
+type MainMenuController() =
+    inherit MonoBehaviour()
+
+    member this.PlayGame() =
+        SceneManager.LoadScene("Gameplay")
+
+    member this.QuitGame() = 
+        Debug.Log("Quitting")
+        Application.Quit()
