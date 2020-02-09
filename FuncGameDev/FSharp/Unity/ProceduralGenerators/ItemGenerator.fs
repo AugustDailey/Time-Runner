a15827653fdfcf98d1aead0a03fd3aa50e65850f
Eugene Kim <kime2@rose-hulman.edu>
2020-02-09 13:50:38 -0500

    Remove validity check in item gen and call spawning in update

---
+++ FuncGameDev/FSharp/Unity/ProceduralGenerators/ItemGenerator.fs
@@ -0,0 +1,16 @@
+﻿module ItemGenerator
+
+let r = new System.Random()
+
+let createItem itemID = 
+    let levelWidth = GameState.instance.level.size |> fst
+    let levelHeight = GameState.instance.level.size |> snd
+    let xRand = r.Next(levelWidth)
+    let yRand = r.Next(levelHeight)
+    let xPos = ((xRand - levelWidth / 2) |> float)
+    let yPos = ((yRand - levelHeight / 2) |> float)
+    Spawner.spawnItem (xPos, yPos) itemID
+    ()
+
+let generateItems items = 
+    items |> List.iter createItem
