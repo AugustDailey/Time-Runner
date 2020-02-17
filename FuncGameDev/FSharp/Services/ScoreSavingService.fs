18e0be3da413da43ad8056286061cfd1dca14606
Eugene Kim <kime2@rose-hulman.edu>
2020-02-17 14:19:03 -0500

    Add file communication in ScoreSaving Service

---
+++ FuncGameDev/FSharp/Services/ScoreSavingService.fs
@@ -0,0 +1,19 @@
+﻿module ScoreSavingService
+
+open System.IO
+
+let getScore = 
+    let baseDirectory = __SOURCE_DIRECTORY__
+    let baseDirectory' = Directory.GetParent(baseDirectory)
+    let filePath = "HighScore.txt"
+    let fullPath = Path.Combine(baseDirectory'.FullName, filePath)
+    let score =  File.ReadAllText(fullPath)
+    score
+
+let storeScore score =
+    let baseDirectory = __SOURCE_DIRECTORY__
+    let baseDirectory' = Directory.GetParent(baseDirectory)
+    let filePath = "HighScore.txt"
+    let fullPath = Path.Combine(baseDirectory'.FullName, filePath)
+    File.WriteAllText(fullPath, score)
+    ()
