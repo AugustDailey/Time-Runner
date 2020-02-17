9379e1fe4bcfd123f6f1ae2745951ffc8c086c79
August B Dailey <daileyab@rose-hulman.edu>
2020-02-17 12:13:37 -0500

    Add modifiability to UI

--- FuncGameDev/FSharp/Unity/MainMenuController.fs
+++ FuncGameDev/FSharp/Unity/MainMenuController.fs
@@ -2,9 +2,52 @@
 
 open UnityEngine
 open UnityEngine.SceneManagement
+open System.Collections.Generic
+open TMPro
 
 type MainMenuController() =
     inherit MonoBehaviour()
+
+    let mutable _keyBindings = new Dictionary<string, KeyCode>()
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _currentKey : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _up : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _down : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _right : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _left : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _attackM : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _attackR : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _roll : GameObject
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _menu : GameObject
+
+    member this.Start() =
+
+
+        _keyBindings.Add("Up", KeyCode.UpArrow)
+        _keyBindings.Add("Down", KeyCode.DownArrow)
+        _keyBindings.Add("Left", KeyCode.LeftArrow)
+        _keyBindings.Add("Right", KeyCode.RightArrow)
+        _keyBindings.Add("Attack(Melee)", KeyCode.G)
+        _keyBindings.Add("Attack(Range)", KeyCode.F)
+        _keyBindings.Add("Roll", KeyCode.R)
+        _keyBindings.Add("Menu", KeyCode.Return)
 
     member this.PlayGame() =
         "Gameplay" |> SceneManager.LoadScene
@@ -13,5 +56,16 @@ type MainMenuController() =
         "Quitting" |> Debug.Log
         Application.Quit()
 
-    member this.ChangeKeyBinding() =
-        "Changing Control" |> Debug.Log
+    member this.OnGUI() = 
+        let e = Event.current
+        if e.isKey then
+            Debug.Log("Uh")
+            _keyBindings.[this._currentKey.name] <- e.keyCode
+            let tm = this._currentKey.GetComponent<TextMeshProUGUI>()
+            tm.text <- e.keyCode.ToString()
+            this._currentKey <- null
+            ()
+
+    member this.ChangeKeyBinding(clicked : GameObject) =
+        this._currentKey <- clicked
+        ()
