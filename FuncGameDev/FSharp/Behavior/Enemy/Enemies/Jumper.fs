05adf0ea5352ef5ccc0830a4e1d3f5c9798f225c
Matthew Lyons <lyonsmj@rose-hulman.edu>
2020-02-18 13:12:46 -0500

    Add Jumper enemy

---
+++ FuncGameDev/FSharp/Behavior/Enemy/Enemies/Jumper.fs
@@ -0,0 +1,33 @@
+﻿module Jumper
+
+let spawn xy bid (gs:GameState.T) =
+    let enemyData = {
+        CommonEntityData.id = gs.nextid;
+        CommonEntityData.position = xy;
+        CommonEntityData.speed = 10.0;
+        CommonEntityData.direction = 0.0;
+        CommonEntityData.isMoving = false;
+        CommonEntityData.data = EntityType.Enemy {
+            health = 10;
+            weapon = {
+                WeaponData.weaponName = "Temp Weapon";
+                WeaponData.cooldown = 0.0;
+                WeaponData.firerate = 0.4;
+                WeaponData.damage = 35;
+                WeaponData.effects = [];
+                WeaponData.weaponType = WeaponData.Category.Melee;
+                WeaponData.behaviorID = 0
+            };
+            effects = [];
+            enemyType = "Jumper";
+            behaviorId = bid;
+            aiId = 3
+        };
+        CommonEntityData.iframes = 0.0;
+        CommonEntityData.sprite = "yay"
+    }
+    { gs with entities = Map.add gs.nextid enemyData gs.entities }
+
+let behavior = {
+    EnemyBehaviorType.spawn = spawn;
+}
---
+++ FuncGameDev/FSharp/Unity/EnemyAI/EnemyAIFunctions/JumperAI.fs
@@ -0,0 +1,16 @@
+﻿module JumperAI
+
+open UnityEngine
+open System
+
+let ai id =
+    // TODO: Modify this to pick a random player or maybe the closest player later on
+    let player = GameStateUtils.getEntityByID GameState.instance 1
+
+    let time = GameState.instance.gamedata.time
+    let isJumping = (((time*2.0 |> int) % 3) = 0)
+    match isJumping with
+    | true ->
+        TrackerAI.ai id
+    | false ->
+        ()
