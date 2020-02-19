0b7729a46ab78f72e2c846e4583a4813b7e454ad
Eugene Kim <kime2@rose-hulman.edu>
2020-02-19 00:32:40 -0500

    Add Game Over screen for when time hits 0 or level reaches 11

---
+++ FuncGameDev/FSharp/Unity/GameOverController.fs
@@ -0,0 +1,24 @@
+﻿namespace FSharp.Unity
+
+open UnityEngine
+open UnityEngine.SceneManagement
+open TMPro
+
+type GameOverController() =
+    inherit MonoBehaviour()
+
+    member this.Start() =
+        let highScore = ScoreSavingService.getScore
+        let txt = this.GetComponentInChildren<TextMeshProUGUI>()
+        txt.text <- "High Score: " + (highScore |> string)
+        ()
+
+    member this.PlayAgain() =
+        "Gameplay" |> SceneManager.LoadScene
+
+    member this.BackToMain() =
+        "MainMenu" |> SceneManager.LoadScene
+
+    member this.QuitGame() = 
+        "Quitting" |> Debug.Log
+        Application.Quit()
--- FuncGameDev/FSharp/Unity/Update.fs
+++ FuncGameDev/FSharp/Unity/Update.fs
@@ -1,9 +1,12 @@
 ﻿namespace FSharp.Unity
 
 open UnityEngine
+open UnityEngine.SceneManagement
 
 type Updater() = 
     inherit MonoBehaviour()
+
+    let highScore = ScoreSavingService.getScore
 
     member this.Start() =
         Spawner.spawnPlayer (GameState.instance.level.stairpos |> fst |> float, GameState.instance.level.stairpos |> snd |> float)
@@ -24,4 +27,20 @@ type Updater() =
         GameState.instance <- GameStateUtils.removeMarkedEntities GameState.instance
         GameState.instance.killIds |> Destroyer.update
         CameraUpdater.update()
+        this.checkGameOver()
+        ()
+
+    member this.checkGameOver() =
+        match GameState.instance.gamedata.floor with 
+        | 11 ->
+            match highScore with
+            | highScore when highScore |> float > GameState.instance.gamedata.time ->
+                ScoreSavingService.storeScore (GameState.instance.gamedata.time |> string)
+            | _ -> ()
+            "GameOver" |> SceneManager.LoadScene
+        | _ -> 
+            match GameState.instance.gamedata.time with
+            | 0.0 -> 
+                "GameOver" |> SceneManager.LoadScene
+            | _ -> ()
         ()
