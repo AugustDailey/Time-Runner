13e03e232d4496b44cb669ad0fc2c21b4f44d95d
August B Dailey <daileyab@rose-hulman.edu>
2020-02-26 18:34:34 -0500

    Add Pause functionality

---
+++ FuncGameDev/FSharp/Unity/PauseMenuController.fs
@@ -0,0 +1,33 @@
+﻿namespace FSharp.Unity
+
+open UnityEngine
+open UnityEngine.SceneManagement
+
+type PauseMenuController() =
+    inherit MonoBehaviour()
+
+    let mutable _isPaused = false
+
+    [<SerializeField>]
+    [<DefaultValue>] val mutable _pauseMenu : GameObject
+
+    member this.Start() =
+        this._pauseMenu.SetActive(false)
+        _isPaused <- false
+
+    member this.Update() =
+        if (not _isPaused && Input.GetKeyDown(KeyCode.Space)) then
+            this.Pause()
+
+    member this.Pause() = 
+        _isPaused <- true
+        this._pauseMenu.SetActive(true)
+        Time.timeScale <- 0.0f
+
+    member this.Play() =
+        _isPaused <- false
+        this._pauseMenu.SetActive(false)
+        Time.timeScale <- 1.0f
+
+    member this.Quit() = 
+        "MainMenu" |> SceneManager.LoadScene
