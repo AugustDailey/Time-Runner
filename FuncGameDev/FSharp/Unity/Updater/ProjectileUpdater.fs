b65ef6b1d9e837d7b623fb2b352042a27945fa43
trdavello <localmgr@davelltr-5510.rose-hulman.edu>
2020-05-05 16:43:25 -0500

    Make projectiles rotate based on direction shot

--- FuncGameDev/FSharp/Unity/Updater/ProjectileUpdater.fs
+++ FuncGameDev/FSharp/Unity/Updater/ProjectileUpdater.fs
@@ -5,5 +5,7 @@ open UnityEngine
 let update (entityData:CommonEntityData.T) (projectileData:ProjectileData.T) (gameObject:GameObject) =
     // update projectiles
     let deltaTime = Time.deltaTime |> float
+    let direction = 0.0f |> float
+    // need to move these just straight forward due to transform.rotate for the sprite
     ProjectileBehavior.decreaseLifespan entityData.id deltaTime |> Commands.addCommand
-    CommonEntityBehavior.move entityData.id entityData.direction |> Commands.addCommand
+    CommonEntityBehavior.move entityData.id direction |> Commands.addCommand
