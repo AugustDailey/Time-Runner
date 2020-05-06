69ea2fa6de4e48c6159909a149deb73ac25e0a5d
August Dailey <daileyab@rose-hulman.edu>
2020-05-06 13:06:01 -0400

    Modify controls for P1 and P2 + refactor update loop

---
+++ FuncGameDev/FSharp/Unity/UpdateLoop.fs
@@ -0,0 +1,20 @@
+﻿module UpdateLoop
+
+open UnityEngine
+
+let Updateloop() = 
+    CameraManager.updateCamera ()
+    GameState.instance <- GameObjectWrapper.wrappers |> CommonEntityUpdater.updateGameStateEntities GameState.instance
+    Time.deltaTime |> float |> GameDataUtils.decreaseTime |> Commands.addCommand
+    GameState.instance <- PlayerUpdater.updatePlayerPos GameState.instance
+    UpdaterDispatcher.issueUpdateCommands GameState.instance GameObjectWrapper.wrappers
+    GameState.instance.entities |> Map.iter UserController.tryQueryInput
+    GameState.instance.entities |> Map.iter EnemyAIScript.callEnemyAI
+    GameState.instance <- Commands.executeAllCommands GameState.instance
+    UpdaterDispatcher.updateAllGameObjects GameState.instance GameObjectWrapper.wrappers
+    Spawner.spawnGameObjects GameState.instance |> Map.iter (fun key value -> GameObjectWrapper.addWrapper value)
+    Generator.tryGenerateLevel GameState.instance
+    GameState.instance <- GameStateUtils.removeMarkedEntities GameState.instance
+    GameState.instance.killIds |> Destroyer.update
+    LevelUpdater.update ()
+    ()
--- FuncGameDev/FSharp/Unity/UpdateMultiplayer.fs
+++ FuncGameDev/FSharp/Unity/UpdateMultiplayer.fs
@@ -19,10 +19,10 @@ type MultiplayerUpdater() =
             ControlModel.up = "up";
             ControlModel.left = "left";
             ControlModel.right = "right";
-            ControlModel.melee = "j";
-            ControlModel.range = "k";
-            ControlModel.active = "l";
-            ControlModel.dodge = ";"
+            ControlModel.melee = "z";
+            ControlModel.range = "x";
+            ControlModel.active = "v";
+            ControlModel.dodge = "c"
         }
 
         let p2cm = {
@@ -30,10 +30,10 @@ type MultiplayerUpdater() =
             ControlModel.up = "w";
             ControlModel.left = "a";
             ControlModel.right = "d";
-            ControlModel.melee = "z";
-            ControlModel.range = "x";
-            ControlModel.active = "v";
-            ControlModel.dodge = "c"
+            ControlModel.melee = "j";
+            ControlModel.range = "k";
+            ControlModel.active = ";";
+            ControlModel.dodge = "l"
         }
 
         Spawner.spawnPlayer (GameState.instance.level.stairpos |> fst |> float, GameState.instance.level.stairpos |> snd |> float) p1cm
@@ -43,20 +43,7 @@ type MultiplayerUpdater() =
         ()
 
     member this.Update() =
-        CameraManager.updateCamera ()
-        GameState.instance <- GameObjectWrapper.wrappers |> CommonEntityUpdater.updateGameStateEntities GameState.instance
-        Time.deltaTime |> float |> GameDataUtils.decreaseTime |> Commands.addCommand
-        GameState.instance <- PlayerUpdater.updatePlayerPos GameState.instance
-        UpdaterDispatcher.issueUpdateCommands GameState.instance GameObjectWrapper.wrappers
-        GameState.instance.entities |> Map.iter UserController.tryQueryInput
-        GameState.instance.entities |> Map.iter EnemyAIScript.callEnemyAI
-        GameState.instance <- Commands.executeAllCommands GameState.instance
-        UpdaterDispatcher.updateAllGameObjects GameState.instance GameObjectWrapper.wrappers
-        Spawner.spawnGameObjects GameState.instance |> Map.iter (fun key value -> GameObjectWrapper.addWrapper value)
-        Generator.tryGenerateLevel GameState.instance
-        GameState.instance <- GameStateUtils.removeMarkedEntities GameState.instance
-        GameState.instance.killIds |> Destroyer.update
-        LevelUpdater.update ()
+        UpdateLoop.Updateloop ()
         CameraUpdater.update ()
         this.checkGameOver ()
         ()
